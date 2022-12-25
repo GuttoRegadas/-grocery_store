8f925745da9fd4c0f4ce95c03fa8b8e5bb431f3f
GuttoRegadas <96195375+GuttoRegadas@users.noreply.github.com>
2022-12-25 20:32:20 -0300

    Modify - 0002
    Models.py

--- Models.py
+++ Models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Category:
     def __init__(self, category):
         self.category = category
@@ -13,3 +16,35 @@ class sStock:
     def __init__(self, products: Products, amount):
         self.products = products
         self.amount = amount
+
+
+class Sale:
+    def __init__(self, unit_sold: Products, seller, buyer, sold_amount, date = datetime.now()):
+        self.unit_sold = unit_sold
+        self.seller = seller
+        self.buyer = buyer
+        self.sold_amount = sold_amount
+        self.date = date
+
+
+class Provider:
+    def __init__(self, name, cnpj, phone, category):
+        self.name = name
+        self.cnpj = cnpj
+        self.phone = phone
+        self.category = category
+
+
+class People:
+    def __init__(self, name, phone, cpf, email, address ):
+        self.name = name
+        self.phone = phone
+        self.cpf = cpf
+        self.email = email
+        self.address = address
+
+
+class Employee(People):
+    def __init__(self, name, phone, cpf, email, address, clt):
+        self.clt = clt
+        super().__init__(name, phone, cpf, email, address)
