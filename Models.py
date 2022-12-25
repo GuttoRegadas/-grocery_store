e9ca6aa3600f79d2e40607b77d3db7c6cec1f6a6
GuttoRegadas <96195375+GuttoRegadas@users.noreply.github.com>
2022-12-25 19:28:06 -0300

    Modify-0001
    Models.py

---
+++ Models.py
@@ -0,0 +1,15 @@
+class Category:
+    def __init__(self, category):
+        self.category = category
+
+class Products:
+    def __init__(self, name, price, category):
+        self.prducts = name
+        self.prducts = price
+        self.prducts = category
+
+
+class sStock:
+    def __init__(self, products: Products, amount):
+        self.products = products
+        self.amount = amount
